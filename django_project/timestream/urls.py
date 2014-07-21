68d08bd015cc73e0cfeb77f02cf312a33b2e02ab
Stephanie Moyerman <smoyerman@gmail.com>
2014-07-21 16:59:20 -0400

    added single uuid recognition to timestream processing

--- django_project/timestream/urls.py
+++ django_project/timestream/urls.py
@@ -7,12 +7,14 @@ urlpatterns = patterns('',
     url(r'^snippets/$', views.SnippetList.as_view()),
     url(r'^devices/$', views.DeviceList.as_view()),
     url(r'^sensors/$', views.SensorList.as_view()),
-    # Individual snippet look to
+    # Individual look for snipper, sensor, device
 	url(r'^snippets/(?P<pk>[0-9]+)/$', views.SnippetDetail.as_view()),
     url(r'^sensors/(?P<pk>[0-9]+)/$', views.SnippetDetail.as_view()),
     url(r'^devices/(?P<pk>[0-9]+)/$', views.SnippetDetail.as_view()),
 	# Timestream look to for a single day
 	url(r'^timestream/$', views.timestream, name='timestream'),
+	url(r'^timestream/(?P<uuid>[^/]+)/$',views.timestream_sensor, name='timestream_sensor'),
+	# User look - shows user info and links to user devices
 	url(r'^users/$', views.UserList.as_view()),
 	url(r'^users/(?P<pk>[0-9]+)/$', views.UserDetail.as_view()),
 
